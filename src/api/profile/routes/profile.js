b9315b2fd45574e311b596e50575147d6c5306b6
Nikola Cvetic <cuekisa@yahoo.com>
2022-04-04 15:03:57 +0200

    profile

--- src/api/profile/routes/profile.js
+++ src/api/profile/routes/profile.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * project router.
+ * profile router.
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::project.project');
+module.exports = createCoreRouter('api::profile.profile');
