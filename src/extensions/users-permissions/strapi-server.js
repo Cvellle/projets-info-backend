927f8e4a73cdd903c229f4c5da159611f309fec2
Nikola Cvetic <cuekisa@yahoo.com>
2022-04-04 15:53:44 +0200

    ver 2

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -1,48 +1,51 @@
 module.exports = (plugin) => {
-  const sanitizeOutput = (user) => {
-    const {
-      password,
-      resetPasswordToken,
-      confirmationToken,
-      ...sanitizedUser
-    } = user; // be careful, you need to omit other private attributes yourself
-    return sanitizedUser;
-  };
+   const sanitizeOutput = (user) => {
+      const {
+         password,
+         resetPasswordToken,
+         confirmationToken,
 
-  plugin.controllers.user.me = async (ctx) => {
-    if (!ctx.state.user) {
-      return ctx.unauthorized();
-    }
-    const user = await strapi.entityService.findOne(
-      "plugin::users-permissions.user",
-      ctx.state.user.id,
-      { populate: ["role"] }
-    );
+         ...sanitizedUser
+      } = user // be careful, you need to omit other private attributes yourself
+      return sanitizedUser
+   }
 
-    ctx.body = sanitizeOutput(user);
-  };
+   plugin.controllers.user.me = async (ctx) => {
+      if (!ctx.state.user) {
+         return ctx.unauthorized()
+      }
+      const user = await strapi.entityService.findOne(
+         'plugin::users-permissions.user',
+         ctx.state.user.id,
+         { populate: ['role'] }
+      )
 
-  plugin.controllers.user.find = async (ctx) => {
-    const users = await strapi.entityService.findMany(
-      "plugin::users-permissions.user",
-      { ...ctx.params, populate: ["role"] }
-    );
+      ctx.body = sanitizeOutput(user)
+   }
 
-    ctx.body = users.map((user) => sanitizeOutput(user));
-  };
+   plugin.controllers.user.find = async (ctx) => {
+      const users = await strapi.entityService.findMany(
+         'plugin::users-permissions.user',
+         { ...ctx.params, populate: ['role'] }
+      )
 
-  plugin.controllers.user.findOne = async (ctx) => {
-    if (!ctx.state.user) {
-      return ctx.unauthorized();
-    }
-    const user = await strapi.entityService.findOne(
-      "plugin::users-permissions.user",
-      ctx.state.user.id,
-      { populate: ["role"] }
-    );
+      ctx.body = users.map((user) => sanitizeOutput(user))
+   }
 
-    ctx.body = sanitizeOutput(user);
-  };
+   plugin.controllers.user.findOne = async (ctx) => {
+      if (!ctx.state.user) {
+         return ctx.unauthorized()
+      }
+      const { id } = ctx.params
 
-  return plugin;
-};
+      const user = await strapi.entityService.findOne(
+         'plugin::users-permissions.user',
+         id,
+         { populate: ['role'] }
+      )
+
+      ctx.body = sanitizeOutput(user)
+   }
+
+   return plugin
+}
